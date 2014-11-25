bfc3e4cf4b7056b8ab5e9faf04946a74d8e98bec
Samuli Piippo <samuli.piippo@theqtcompany.com>
2014-11-25 09:17:25 +0200

    Helper script to build all device images
    build-all.sh can be used to build image and toolchain for all
    device which have build environment already setup. Have to use bash,
    since the oe-init-build-env does not support dash.
    
    Change-Id: I95778919015b40444f229d471791b5637de7d0d3
    Reviewed-by: Eirik Aavitsland <eirik.aavitsland@theqtcompany.com>

---
+++ scripts/build-all.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#############################################################################
+##
+## Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies).
+##
+## This file is part of the Qt Enterprise Embedded Scripts of the Qt
+## framework.
+##
+## $QT_BEGIN_LICENSE$
+## Commercial License Usage Only
+## Licensees holding valid commercial Qt license agreements with Digia
+## with an appropriate addendum covering the Qt Enterprise Embedded Scripts,
+## may use this file in accordance with the terms contained in said license
+## agreement.
+##
+## For further information use the contact form at
+## http://www.qt.io/contact-us.
+##
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+echo "-------------------------------------" >> build.log
+for DIR in $(ls -d build-*); do
+    (
+    export MACHINE=${DIR#*-}
+    . ./setup-environment.sh
+
+    echo "${MACHINE}:" >> ../build.log
+    echo "  start: $(date)" >> ../build.log
+    bitbake b2qt-embedded-image meta-toolchain-b2qt-embedded-sdk || echo "    build failed" >> ../build.log
+    echo "  end:   $(date)" >> ../build.log
+    )
+done
