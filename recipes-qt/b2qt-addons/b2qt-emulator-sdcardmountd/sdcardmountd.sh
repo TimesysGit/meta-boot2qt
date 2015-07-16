7b7b6b9f369a2b707e8ed9334e0d81cdb588e2b1
Rainer Keller <rainer.keller@theqtcompany.com>
2015-07-16 10:33:14 +0200

    Add Emulator sdcardmountd
    Change-Id: Ib760d0c25c645b1c44216081c42714f8e9a8ca32
    Reviewed-by: Samuli Piippo <samuli.piippo@theqtcompany.com>

---
+++ recipes-qt/b2qt-addons/b2qt-emulator-sdcardmountd/sdcardmountd.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#############################################################################
+##
+## Copyright (C) 2015 Digia Plc and/or its subsidiary(-ies).
+##
+## This file is part of the Qt Enterprise Embedded Scripts of the Qt
+## framework.
+##
+## $QT_BEGIN_LICENSE$
+## Commercial License Usage Only
+## Licensees holding valid commercial Qt license agreements with Digia
+## with an appropriate addendum covering the Qt Enterprise Embedded Scripts,
+## may use this file in accordance with the terms contained in said license
+## agreement.
+##
+## For further information use the contact form at
+## http://www.qt.io/contact-us.
+##
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+DAEMON=/usr/bin/sdcardmountd
+
+case "$1" in
+start)
+    start-stop-daemon --start --quiet --exec $DAEMON &
+    ;;
+stop)
+    start-stop-daemon --stop --quiet --exec $DAEMON
+    ;;
+restart)
+    start-stop-daemon --stop --quiet --exec $DAEMON
+    sleep 1
+    start-stop-daemon --start --quiet --exec $DAEMON &
+    ;;
+*)
+    echo "Usage: $0 {start|stop|restart}"
+    exit 1
+esac
+exit 0
